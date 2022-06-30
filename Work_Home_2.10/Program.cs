dc0a247060cd7a1ecba3f6a6d0eeb4ab0061f826
Maksim <bagzmax75@gmail.com>
2022-07-01 00:29:12 +0300

    add Work_Hom2.1

---
+++ Work_Home_2.10/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 100 && number < 1000) // число трехзначное или нет
+{
+    System.Console.WriteLine(number /10 % 10);
+}
+else
+{
+  System.Console.WriteLine("Введите трёхзначное число!");  
+}
+
