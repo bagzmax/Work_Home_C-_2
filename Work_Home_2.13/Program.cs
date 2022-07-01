99ff9cb2aac9a7fdc1f27d80c435378a90cbdd11
Maksim <bagzmax75@gmail.com>
2022-07-01 20:38:37 +0300

    add Work Home 2.13

---
+++ Work_Home_2.13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+if (int.Parse (number) > 99)
+{
+    Console.WriteLine("Третья цифра числа: " + number[2]);
+}
+else
+{
+   Console.WriteLine("Третьей цифры нет"); 
+}
